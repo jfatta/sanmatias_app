5af465eb33c8e9f762bba174f5e3fdcf40785ba4
Jorge L. Fatta <jorgefatta@gmail.com>
2025-03-11 22:37:43 -0300

    change font

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import '../styles/globals.css'
 import Analytics from "../scripts/analytics"
+import { GeistSans } from 'geist/font/sans'
+import { GeistMono } from 'geist/font/mono'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-    <Analytics />
-    <Component {...pageProps} />
-    </>
+    <main className={`${GeistSans.variable} ${GeistMono.variable} font-sans`}>
+      <Analytics />
+      <Component {...pageProps} />
+    </main>
   )
 }
 
